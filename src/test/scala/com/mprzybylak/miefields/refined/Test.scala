54d5e9de751b80cf7882f06ad0ab9060dd1210f0
Michał Przybylak <michal.przybylak@gmail.com>
2019-12-22 17:54:10 +0100

    extended hello world example

--- src/test/scala/com/mprzybylak/miefields/refined/Test.scala
+++ src/test/scala/com/mprzybylak/miefields/refined/Test.scala
@@ -4,14 +4,40 @@ import eu.timepit.refined._
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.auto._
 import eu.timepit.refined.numeric._
+import org.scalatest.matchers.should.Matchers._
 
 import org.scalatest.wordspec.AnyWordSpec
 
 class Test extends AnyWordSpec {
 
-  "compile time check for positive number" in {
-    val positive: Int Refined Positive = 1
-//    val negative: Int Refined Positive = -1 // will not compile
+  val left = Symbol("left")
+  val right = Symbol("right")
+
+  "Literals validation" should {
+
+    "compile time check for positive number" in {
+      val positive: Int Refined Positive = 1
+      //    val negative: Int Refined Positive = -1 // will not compile
+    }
+
+    "compile time check for positive number using refineMV macro" in {
+      val positive = refineMV[Positive](2)
+      //    val negative = refineMV[Positive](-2) // will not compile
+    }
+  }
+
+  "Values validation" should {
+
+    "runtime check for positive number" in {
+      val p = 1
+      val positive = refineV[Positive](p)
+      positive should be(right)
+
+      val n = -1
+      val notPositive = refineV[Positive](n)
+      notPositive should be(left)
+    }
+
   }
 
 }
